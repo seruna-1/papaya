5e236feb0372fbabd4d17b7df2a8c1b1f6a1a449
Mateus Cezario Barreto <mateus.cezario.barreto@gmail.com>
2025-03-19 15:18:02 -0300

    [ [tools/build.py] seems to work for markdown source files. Files bult in a branch created for documentation seem ok ]

--- documentation/markdown/en-US/tools/build.py/insides.md
+++ documentation/markdown/en-US/tools/build.py/insides.md
@@ -1,4 +1,4 @@
-# Insides
+<title> Insides </title>
 
 One class: [Builder], responsible for building the documentation. As input, constructor takes:
 
@@ -14,7 +14,26 @@ From each source file, will be generated a destination file at the same relative
 
 # Method Builder.build
 
+A model HTML file is read and its text is stored.
+
 [Path.walk()] is used to select each directory inside source root directory. If there is at least one relevant source file directly inside the current source directory, the relative path to that directory from the source root directory is replicated in destination root directory.
 
 The relevant source files are read, built and writed to destination.
 
+If a relevant file is an HTML file, its text is passed directly to method [Builder.build_from_html], resulting in a text ready to be writed.
+
+If a relevant file is not an HTML file, its text is transformed to HTML format before proceding to pass it to method [Builder.build_from_html].
+
+The model HTML text is parsed and BeautifulSoup object is passed to [Builder.build_from_html]. This includes a slightly overhead, as the object is recreated from model text for each time [Builder.build_from_html] is called.
+
+# Method Builder.build_from_html
+
+The HTML text received as input is prefixed with
+
+`<main>`
+
+And suffixed with
+
+`</main>`
+
+A parsed format of it is created and replaces the element [main] in template. If there is a element [title] inside a element [meta] inside a element [head], it is ripped and replaces the title of the template.
--- tools/build.py
+++ tools/build.py
@@ -1,6 +1,8 @@
-from pathlib import Path
+import markdown
 
 import argparse
+
+from pathlib import Path
 
 from bs4 import BeautifulSoup
 
@@ -14,19 +16,27 @@ class Builder:
 
 		self.source_format = source_format
 
+		self.kaki_relative_path_from_top = Path( kaki_relative_path )
+
 		match self.source_format:
 
 			case 'html' : self.relevant_file_extension = '.html'
 
 			case 'markdown' : self.relevant_file_extension = '.md'
 
+			case _ : raise Exception( "Not a valid source format." )
+
 		return
 
 	def build ( self ) :
 
-		for source_directory_path, source_directory_names, source_file_names in Path( self.source_root ).walk() :
+		model_path = Path( __file__ ).parent / 'model.html'
 
-			is_parent_relocated = False
+		model_text = model_path.read_text()
+
+		for source_directory_path, source_directory_names, source_file_names in self.source_root.walk() :
+
+			parent_was_relocated = False
 
 			for source_file_name in source_file_names :
 
@@ -34,27 +44,69 @@ class Builder:
 
 					source_file_path = source_directory_path / source_file_name
 
-					destination_file_path = Path(
-						str( source_file_path ).replace( self.source_root, self.destination_root )
-					)
+					destination_file_path = self.destination_root / source_file_path.relative_to( self.source_root ).with_suffix( ".html" )
 
-					if is_parent_relocated == False : destination_file_path.parent.mkdir() ; is_parent_relocated = True
+					self.kaki_relative_path = self.destination_root.relative_to( destination_file_path.parent, walk_up=True ) / self.kaki_relative_path_from_top
 
-					if self.source_format != 'html' : print( "Error. Not html." ) ; return
+					if parent_was_relocated == False : destination_file_path.parent.mkdir( parents=True, exist_ok=True ) ; parent_was_relocated = True
 
-					html_text = source_file_path.read_text()
+					text = source_file_path.read_text()
 
-					html_text = self.build_from_html( html_text )
+					if self.source_format == 'markdown' :
 
-					destination_file_path.write_text( html_text )
+						text = markdown.markdown( text )
+
+					model = BeautifulSoup( model_text, 'html.parser' )
+
+					built = self.build_from_html( text, model )
+
+					destination_file_path.write_text( built )
 
 		return
 
-	def read_from_html ( text ) :
+	def build_from_html ( self, text, model ) :
 
-		result = "This is just a text.\n"
+		text = '<main>' + text + '</main>'
 
-		return result
+		model.find( 'main' ).replace_with( BeautifulSoup( text, 'html.parser' ).main )
+
+		result = model
+
+		given_titles = result.select( 'main title' )
+
+		if given_titles != [] : result.html.head.title.replace_with( given_titles[0] )
+
+		result = self.generate_references_to_kaki( result )
+
+		return result.prettify()
+
+	def generate_references_to_kaki ( self, soup ) :
+
+		kaki_css_path = self.kaki_relative_path / 'main.css'
+
+		kaki_javascript_path = self.kaki_relative_path / 'main.js'
+
+		elements_link_stylesheet = soup.head.find_all( 'link', rel='stylesheet' )
+
+		assert ( elements_link_stylesheet != None ) , "Could not find any element [link] with attribute [rel] set to [stylesheet]."
+
+		for element in elements_link_stylesheet :
+
+			refers_to_kaki = 'kaki' in element['href']
+
+			if refers_to_kaki == True : element['href'] = str( kaki_css_path ) ; break
+
+		elements_script = soup.find_all( 'script' )
+
+		assert ( elements_script != None ) , "Could not find any element [script]."
+
+		for element_script in elements_script :
+
+			refers_to_kaki = ( 'src' in element_script.attrs ) and ( 'kaki' in element_script['src'] )
+
+			if  refers_to_kaki == True : element_script['src'] = str( kaki_javascript_path ) ; break
+
+		return soup
 
 if __name__ == '__main__':
 
@@ -64,12 +116,17 @@ if __name__ == '__main__':
 
 	option_parser.add_argument( "--source-format", dest='source_format', required=True )
 
-	option_parser.add_argument( "--destination" )
+	option_parser.add_argument( "--destination", required=True )
 
-	option_parser.add_argument( "--kaki" )
+	option_parser.add_argument( "--kaki", default="./kaki" )
 
 	options = option_parser.parse_args()
 
-	builder = Builder( source=options.source, source_format=options.source_format, destination=options.destination, kaki=options.kaki )
+	builder = Builder(
+		source_root=options.source,
+		source_format=options.source_format,
+		destination_root=options.destination,
+		kaki_relative_path=options.kaki
+	)
 
 	builder.build()
